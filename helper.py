31449c247171d211123fdfb995fe8a4fe2f1a689
Rituraj <agr.rituraj@gmail.com>
2025-01-26 18:30:23 +0530

    added profile checking function

--- helper.py
+++ helper.py
@@ -1,5 +1,6 @@
 import discord
 from constant import ME
+from models import Profile
 
 
 async def send_error(file, function_name, error, client, server='FragPunk'):
@@ -7,4 +8,15 @@ async def send_error(file, function_name, error, client, server='FragPunk'):
     embed.add_field(name=function_name, value=error, inline=False)
     user = await client.fetch_user(ME)
     await user.send(embed=embed)
+
+
+async def check_profile(discord_id, discord_name):
+    # Checking for user's profile
+    user = await Profile.get_or_none(discord_id=str(discord_id))
+    if not user:
+        # Creating user profile
+        user = Profile(discord_id=str(discord_id), discord_name=str(discord_name))
+        await user.save()
+
+    return user
 
