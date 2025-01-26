4dfcc97f7dd1ecc61a39de36074db4450beb3ce7
Rituraj <agr.rituraj@gmail.com>
2025-01-26 17:14:06 +0530

    added a event model for all events and added card model for card event

--- models.py
+++ models.py
@@ -13,6 +13,33 @@ class Profile(Model):
         return self.discord_name
 
 
+# EVENTS
+
+class Event(Model):
+    # Primary Key
+    id = fields.IntField(primary_key=True)
+    # Information
+    name = fields.CharField(max_length=100)
+    description = fields.TextField(null=True)
+    start_time = fields.DatetimeField(null=True)
+    end_time = fields.DatetimeField(null=True)
+    archived = fields.BooleanField(default=False)
+
+
+# DRAW CARD EVENT MODELS
+
+class Card(Model):
+    # Primary key
+    id = fields.IntField(primary_key=True)
+    # Card details
+    name = fields.CharField(max_length=100)
+    description = fields.TextField()
+    image = fields.CharField(max_length=225)
+    # User who got the card
+    user = fields.ManyToManyField("models.Profile", related_name="cards", on_delete=fields.CASCADE)
+
+
+# CHRISTMAS EVENT MODEL
 class ChristmasResponseEvent(Model):
     id = fields.IntField(primary_key=True)
     user_id = fields.ForeignKeyField("models.Profile", on_delete=fields.CASCADE)
